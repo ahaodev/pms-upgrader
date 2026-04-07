f98e0a39dc9d5ce8a811fe08e0dddccf9237d3b1
ahaodev <52927295@qq.com>
2026-04-07 22:43:00 +0800

    ignore

--- upgrader/src/main/java/com/ahao/upgrader/ServerCreator.kt
+++ upgrader/src/main/java/com/ahao/upgrader/ServerCreator.kt
@@ -1,5 +1,6 @@
 package com.ahao.upgrader
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
